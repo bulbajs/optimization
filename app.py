2140f14b26c9862855914328415216be83405522
bulbajs <artval.kuptsov@gmail.com>
2026-03-01 22:53:09 +0300

    edited file
    added alertdialog

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-# import tkinter as tk
-#
-# window = tk.Tk()
-# # window.title('Оптимизация')
-#
-# EXW = tk.Label(window, text='ff')
-# EXW.pack()
-# window.mainloop()
-
 import flet as ft
 from optimization import optimization
 
@@ -14,21 +5,43 @@ def main(page: ft.Page):
     page.title = 'OPTIMIZATION'
     exw_field = ft.TextField(label = "EXW", hint_text = "Введите значение")
     tr_field = ft.TextField(label="TR", hint_text="Введите значение")
-    custom_rate_field = ft.TextField(label="customs_rate", hint_text="Введите 0.22 или 0.32")
+    customs_rate_field = ft.TextField(label="customs_rate", hint_text="Введите 0.22 или 0.32")
     result = ft.Text(value = '0')
+    dlg = ft.AlertDialog(
+        modal= True,
+        title=ft.Text('Внимание'),
+        content=ft.Text('Введите корректное значение'),
+        actions=[],
+    )
+
+    def dlg_close(e):
+        page.pop_dialog()
+
+    dlg.actions.append(ft.TextButton(content = 'OK', on_click = dlg_close))
+
+
     def calculate(e):
-        result.value = exw_field.value
+        try:
+            EXW = int(exw_field.value)
+            TR = int(tr_field.value)
+            cm_rate = float(customs_rate_field.value)
+            res = optimization(EXW, TR, cm_rate)
+            a = res['customer_price']
+            result.value = f'Цена для клиента  = {a:.2f} евро'
+        except ValueError:
+                page.show_dialog(dlg)
+                return
         page.update()
 
     page.add(
         ft.Text(value = 'Давайте посчитаем', size = 22, weight=ft.FontWeight.W_600, color = '#0F4F11'),
         exw_field,
         tr_field,
-        custom_rate_field,
+        customs_rate_field,
         result,
         ft.Button(content = 'Calculate', on_click = calculate ),
         )
-
+    page.dialog = dlg
 
 
 
